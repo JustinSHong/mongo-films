77fe68e7ffed570013398769fbe7b2355f5092e3
Justin Hong <jhong2009@gmail.com>
2018-05-11 00:07:33 -0700

    in Character.js: each character's Card only renders personal data

--- client/src/components/Character.js
+++ client/src/components/Character.js
@@ -4,31 +4,37 @@ import { Card, CardHeader, CardTitle, CardText } from "material-ui/Card";
 // styles
 import "../styles/character.css";
 
-const Character = props => {
-	console.log("CHARACTER PROPS: ", props);
-	return (
-		<Card className="Character">
-			<CardHeader>
-				<CardTitle title={props.char.name} />
-			</CardHeader>
-			{Object.keys(props.char).map(trait => {
-				// only render those character traits that are known
-				// don't render miscellaneous traits
-				if (
-					props.char[trait] !== "unknown" &&
-					trait !== "name" &&
-					trait !== "_id" &&
-					trait !== "created" &&
-					trait !== "edited" &&
-					trait !== "key" &&
-					trait !== "__v" &&
-					trait !== "homeworld_key"
-				) {
-					return <CardText>{props.char[trait]}</CardText>;
-				}
-			})}
-		</Card>
-	);
-};
+class Character extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		console.log("CHARACTER PROPS: ", this.props);
+		return (
+			<Card className="Character">
+				<CardHeader>
+					<CardTitle title={this.props.char.name} />
+				</CardHeader>
+				{Object.keys(this.props.char).map(trait => {
+					// only render those character traits that are known
+					// don't render miscellaneous traits
+					if (
+						this.props.char[trait] !== "unknown" &&
+						trait !== "name" &&
+						trait !== "_id" &&
+						trait !== "created" &&
+						trait !== "edited" &&
+						trait !== "key" &&
+						trait !== "__v" &&
+						trait !== "homeworld_key"
+					) {
+						return <CardText>{this.props.char[trait]}</CardText>;
+					}
+				})}
+			</Card>
+		);
+	}
+}
 
 export default Character;
